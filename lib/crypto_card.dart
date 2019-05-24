a6e611bc7b5903a736673191b67f187a2b982eea
Dilip <dilip_agheda@yahoo.com>
2019-05-24 21:51:59 +1000

    seperated out a component for crypto card

---
+++ lib/crypto_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CryptoCard extends StatelessWidget {
+  final String lastPrice;
+  final String selectedCurrency;
+  final String crypto;
+  CryptoCard({this.lastPrice, this.selectedCurrency, this.crypto});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $crypto = $lastPrice $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
